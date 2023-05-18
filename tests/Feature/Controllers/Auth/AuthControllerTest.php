70a8a3b14a648e90292b3eb1e96f738cd5a4eda1
GianZapata <elgianmc@hotmail.com>
2023-05-18 17:03:38 -0600

    test

--- tests/Feature/Controllers/Auth/AuthControllerTest.php
+++ tests/Feature/Controllers/Auth/AuthControllerTest.php
@@ -3,17 +3,22 @@
 namespace Tests\Feature\Controllers\Auth;
 
 use App\Models\User;
+use App\Models\UserProfile;
+use Carbon\Carbon;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Hash;
 use Tests\TestCase;
 
 class AuthControllerTest extends TestCase
 {
+    // use RefreshDatabase;
+
     /**
      * A basic feature test example.
      */
 
-     public function test_the_application_check_email_route_returns_true_if_email_exists(): void {
+    public function test_the_application_check_email_route_returns_true_if_email_exists(): void {
 
         /** @var User $user */
         $user = User::factory()->create();
