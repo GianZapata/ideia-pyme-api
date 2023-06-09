8bed4b43a18f8e7f0e28d8caf563742733ff5ca0
Antwan Hernandez <jorge.hernandezcrtz@uanl.edu.mx>
2023-06-09 12:18:33 -0600

    antiguedad clientes

--- app/Http/Controllers/Reports/ClientsController.php
+++ app/Http/Controllers/Reports/ClientsController.php
@@ -15,15 +15,59 @@ class ClientsController extends Controller {
 
     public function obtenerTop5Clientes()
 {
-    $top5Clientes = Emisor::select('facturas.receptor_id', DB::raw('COUNT(*) AS total_facturas'), DB::raw('MAX(receptores.rfc) AS rfc'), 'receptores.nombre', DB::raw('SUM(comprobantes.total) AS total'), 'comprobantes.moneda')
-        ->leftJoin('facturas', 'emisores.id', '=', 'facturas.emisor_id')
-        ->leftJoin('comprobantes', 'facturas.id', '=', 'comprobantes.factura_id')
-        ->leftJoin('receptores', 'facturas.receptor_id', '=', 'receptores.id')
-        ->where('emisores.rfc', 'ACO091214PD0')
-        ->groupBy('facturas.receptor_id', 'receptores.nombre', 'comprobantes.moneda')
-        ->orderBy('total_facturas', 'DESC')
-        ->limit(5)
-        ->get();
+
+    $top5Clientes = Emisor::select(
+        'facturas.receptor_id',
+        DB::raw('COUNT(*) AS total_facturas'),
+        DB::raw('MAX(receptores.rfc) AS rfc'),
+        'receptores.nombre',
+        // DB::raw('GROUP_CONCAT(comprobantes.moneda SEPARATOR ",") AS monedas'),
+        DB::raw('SUM(comprobantes.total) AS total'),
+        DB::raw('MIN(comprobantes.fecha) AS fecha_minima'),
+        DB::raw('MAX(comprobantes.fecha) AS fecha_maxima'),
+        DB::raw('TIMESTAMPDIFF(MONTH, MIN(comprobantes.fecha), CURDATE()) / 12 AS antiguedad'),
+        DB::raw('CASE WHEN TIMESTAMPDIFF(MONTH, MAX(comprobantes.fecha), CURDATE()) > 1 THEN "noactivo" ELSE "activo" END AS estado')
+    )
+    ->leftJoin('facturas', 'emisores.id', '=', 'facturas.emisor_id')
+    ->leftJoin('comprobantes', 'facturas.id', '=', 'comprobantes.factura_id')
+    ->leftJoin('receptores', 'facturas.receptor_id', '=', 'receptores.id')
+    ->where('emisores.rfc', 'ACO091214PD0')
+    ->groupBy('facturas.receptor_id', 'receptores.nombre')
+    ->orderBy('total_facturas', 'DESC')
+    ->limit(5)
+    ->get();
+
+
+    // $top5Clientes = Emisor::select('facturas.receptor_id', DB::raw('COUNT(*) AS total_facturas'), DB::raw('MAX(receptores.rfc) AS rfc'), 'receptores.nombre', DB::raw('GROUP_CONCAT(comprobantes.moneda SEPARATOR ",") AS monedas'), DB::raw('SUM(comprobantes.total) AS total'))
+    // ->leftJoin('facturas', 'emisores.id', '=', 'facturas.emisor_id')
+    // ->leftJoin('comprobantes', 'facturas.id', '=', 'comprobantes.factura_id')
+    // ->leftJoin('receptores', 'facturas.receptor_id', '=', 'receptores.id')
+    // ->where('emisores.rfc', 'ACO091214PD0')
+    // ->groupBy('facturas.receptor_id', 'receptores.nombre')
+    // ->orderBy('total_facturas', 'DESC')
+    // ->limit(5)
+    // ->get();
+
+
+    // $top5Clientes = Emisor::select('facturas.receptor_id', DB::raw('COUNT(*) AS total_facturas'), DB::raw('MAX(receptores.rfc) AS rfc'), 'receptores.nombre')
+    //     ->leftJoin('facturas', 'emisores.id', '=', 'facturas.emisor_id')
+    //     ->leftJoin('comprobantes', 'facturas.id', '=', 'comprobantes.factura_id')
+    //     ->leftJoin('receptores', 'facturas.receptor_id', '=', 'receptores.id')
+    //     ->where('emisores.rfc', 'ACO091214PD0')
+    //     ->groupBy('facturas.receptor_id', 'receptores.nombre')
+    //     ->orderBy('total_facturas', 'DESC')
+    //     ->limit(5)
+    //     ->get();
+
+    // $top5Clientes = Emisor::select('facturas.receptor_id', DB::raw('COUNT(*) AS total_facturas'), DB::raw('MAX(receptores.rfc) AS rfc'), 'receptores.nombre', DB::raw('SUM(comprobantes.total) AS total'), 'comprobantes.moneda')
+    //     ->leftJoin('facturas', 'emisores.id', '=', 'facturas.emisor_id')
+    //     ->leftJoin('comprobantes', 'facturas.id', '=', 'comprobantes.factura_id')
+    //     ->leftJoin('receptores', 'facturas.receptor_id', '=', 'receptores.id')
+    //     ->where('emisores.rfc', 'ACO091214PD0')
+    //     ->groupBy('facturas.receptor_id', 'receptores.nombre', 'comprobantes.moneda')
+    //     ->orderBy('total_facturas', 'DESC')
+    //     ->limit(5)
+    //     ->get();
 
     return response()->json([
         'top5clientes' => $top5Clientes,
